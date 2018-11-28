622621a79873e6713dfb98658c028e3890623a44
Subhajeet Mukherjee <33711869+bootkernel@users.noreply.github.com>
2018-11-28 14:24:36 -0800

    Add files via upload

---
+++ fizzbuzz_community_edition.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+i=1
+# A simple while loop
+while i <= 100:
+    # % denotes remainder. So, if the remainder of 3 is 0 it'll print Fizz
+    if i%3==0:
+	# end='something', ends the print function output with something
+        print("Fizz", end='')
+        if i%5==0:
+            print("_Buzz", end='')
+    # Else if statement
+    elif i%5==0:
+        print("Buzz", end='')
+    else:
+        print(i, end='')
+    print()
+    i = i + 1
